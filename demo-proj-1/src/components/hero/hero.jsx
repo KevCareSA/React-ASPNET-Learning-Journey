d126b4b99e012fed60f90540275309529a9db0f1
KevCareSA <kev.mkbn2@gmail.com>
2025-06-21 21:55:52 +0200

    feat: add black.png asset and update hero component

--- demo-proj-1/src/components/hero/hero.jsx
+++ demo-proj-1/src/components/hero/hero.jsx
@@ -11,21 +11,31 @@ function hero() {
           <div className="grid grid-cols-1 md:grid-cols-2 
           lg:grid-cols-3 place-items-center min-h-[850px]">
             {/* text section */}
-            <div className="text-lightOrange mt-[100px]">
+            <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
               <h1 className="text-7xl font-bold leading-tight ml-14">
                 Blvck Tumbler
               </h1>
-              <div className="relative z-10 space-y-4">
-                <h1> Black Lifestyle Lovers, </h1>
-                <h1 className="text-sm opacity-55 leading-loose">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Numquam molestiae fuga quis impedit suscipit sequi. Error
-                  aliquam, laborum voluptate voluptatum amet explicabo. Commodi
-                  cumque beatae voluptate, incidunt nemo mollitia tempore!
-                </h1>
+              
+              <div className="relative">
+                <div className="relative z-10 space-y-4">
+                  <h1> Black Lifestyle Lovers, </h1>
+                  <h1 className="text-sm opacity-55 leading-loose">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Numquam molestiae fuga quis impedit suscipit sequi. Error
+                    aliquam, laborum voluptate voluptatum amet explicabo. Commodi
+                    cumque beatae voluptate, incidunt nemo mollitia tempore!
+                  </h1>
+                </div>
+              </div>
+
+              <div className="absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25">
+
               </div>
             </div>
             {/* Hero Text */}
+            <div className ="relative">
+              <img src={}>
+            </div>
             {/* 3rd dive */}
           </div>
         </div>
