66cb9c79d0b085714dea7a263c10cc385be56ac4
KevCareSA <kev.mkbn2@gmail.com>
2025-06-24 22:22:16 +0200

    chore(config): changed  the configuraion due to bug on tailwind

--- demo-proj-1/components/Hero.jsx
+++ demo-proj-1/components/Hero.jsx
@@ -42,6 +42,7 @@ function Hero() {
         </div>
       </section>
     </main>
+
   );
 }
 
--- demo-proj-1/src/App.jsx
+++ demo-proj-1/src/App.jsx
@@ -1,20 +1,14 @@
-import { motion } from 'framer-motion';
 import React from 'react';
-import Hero from './components/Hero';
+import Hero from '../components/Hero';
 
 
 function App() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-      className="bg-red-600 text-white text-4xl font-bold p-10 rounded-lg shadow-lg"
-    >
-      Tailwind + Framer Motion works! 🚀
-
+    <div className="overflow-x-hidden">
       <Hero />
-    </motion.div>
+    
+    </div>
+  
   );
 }
 
