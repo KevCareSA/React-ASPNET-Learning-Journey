28ae77f8df95f66f1fd6a72690b42546c389d9b3
KevCareSA <kev.mkbn2@gmail.com>
2025-06-27 22:21:26 +0200

    feat: add [Hero Component, improving Taiilwind features]

--- demo-proj-1/components/Hero.jsx
+++ demo-proj-1/components/Hero.jsx
@@ -12,43 +12,54 @@ const bgImage = {
 
 
 
-function Hero() {
+const Hero = () => {
   return (
+
     <main style={bgImage}>
       <section className="min-h-[750px] w-full">
         <div className="container">
           {/* Navbar Section*/}
           {/* Hero Section */}
 
-          <div className="grid grid-cols-1 md:grid-cols-2 
-          lg:grid-cols-3 place-items-center min-h-[850px]">
-            {/* text section */}
-            <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
-              <h1 className="text-7xl font-bold leading-tight ml-14">
-                Blvck Coffee
-              </h1>
-              
-              <div className="relative">
-                <div className="relative z-10 space-y-4">
-                  <h1> Black Lifestyle Lovers, </h1>
-                  <h1 className="text-sm opacity-50 leading-loose">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Numquam molestiae fuga quis impedit suscipit sequi. Error
-                    aliquam, laborum voluptate voluptatum amet explicabo. 
-                    Commodi cumque beatae voluptate, incidunt nemo mollitia 
-                    tempore!
-                  </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
+           place-items-center min-h-[850px]">
+            <div className="grid grid-cols-1 bg-green-100 border border-black">
+              {/* text section */}
+              <div className="text-orange-400 mt-[100px] md:mt-0 p-4 space-y-28">
+                <h1 className="text-7xl font-bold leading-tight ml-14">
+                  Blvck Coffee
+                </h1>
+                
+                <div className="relative">
+                  <div className="relative z-10 space-y-4">
+                    <h1> Black Lifestyle Lovers, </h1>
+                    <h1 className="text-sm opacity-50 leading-loose">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Numquam molestiae fuga quis impedit suscipit sequi. Error
+                      aliquam, laborum voluptate voluptatum amet explicabo. 
+                      Commodi cumque beatae voluptate, incidunt nemo mollitia 
+                      tempore!
+                    </h1>
+                  </div>
+                </div>
+
+                <div className="absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25">
+
                 </div>
               </div>
-
-              <div className="absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25">
-
-              </div>
             </div>
-            {/* Hero Text */}
+            {/* Hero Section */}
             <div className="relative">
-              <img src={BlackCoffee} />
+              <img src={BlackCoffee} alt="" className="relative z-40 h-[400px] 
+              md:h-[700px] img-shadow"/>
             </div>
+            {/* Orange Ring Circle */}
+            <div className="h-[180px] w-[180px] absolute top-24 -right-16 
+            border-primary border-[20px] rounded-full z-10"></div>
+            <div className="text-[140px] scale-150 font-bold text-dark-orange">
+              Blvck Coffee
+            </div>
+
             {/* 3rd dive */}
           </div>
         </div>
@@ -59,3 +70,16 @@ function Hero() {
 }
 
 export default Hero;
+
+
+// import React from 'react';
+
+// export default function Hero() {
+//   return (
+//     <div className="bg-yellow-200 p-10">
+//       <h1 className="text-4xl font-bold underline text-green-600">
+//         ✅ Tailwind IS NOW working inside Hero.jsx
+//       </h1>
+//     </div>
+//   );
+// }
