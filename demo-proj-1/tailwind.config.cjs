5e253727fc67db7ffc1d5fb231256b0fbeebf35d
KevCareSA <kev.mkbn2@gmail.com>
2025-06-28 16:34:07 +0200

    fix: resolve Tailwind not applying styles by correcting file structure

--- demo-proj-1/tailwind.config.cjs
+++ demo-proj-1/tailwind.config.cjs
@@ -2,10 +2,10 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}", // <-- MAKE SURE `.jsx` is here
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
