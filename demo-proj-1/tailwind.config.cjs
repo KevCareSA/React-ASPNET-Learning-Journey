76b0e38e0d3d6dd11e361492868395a0d7733a51
KevCareSA <kev.mkbn2@gmail.com>
2025-06-29 23:43:25 +0200

    feat(Tailwind): Added background colours

--- demo-proj-1/tailwind.config.cjs
+++ demo-proj-1/tailwind.config.cjs
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // <-- MAKE SURE `.jsx` is here
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f19509",
+        primaryDark : "#e86f00",
+        lightOrange: "#f1dabf",
+        darkOrange: "#1a1f25",
+        green: "#272c35",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+        },
+    },
   },
+},
   plugins: [],
 };
