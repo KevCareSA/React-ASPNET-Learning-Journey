4e3b6b71e806b09088df977a520f3bb88a86756a
KevCareSA <kev.mkbn2@gmail.com>
2025-07-02 14:09:20 +0200

    feat: add alignment code and update hero component

--- demo-proj-1/src/components/Hero.jsx
+++ demo-proj-1/src/components/Hero.jsx
@@ -57,21 +57,47 @@ const Hero = () => {
             <div className="relative">
               <img src={BlackCoffee} alt="" className="relative z-40 h-[400px] 
               md:h-[700px] img-shadow"/>
-            </div>
+            
             {/* Orange Ring Circle */}
-            <div className="h-[180px] w-[180px] absolute top-24 left-[51%] 
+             <div className="h-[180px] w-[180px] absolute top-24 left-[51%] 
             border-primary border-[20px] rounded-full z-10"></div>
             
             {/* Big Text Section */}
-            <div>
-              <h1 className="text-[140px] scale-120 font-bold text-darkGray/40
-            leading-none">
-              Blvck Coffee
-              </h1>
+              <div className="absolute -top-20 left-[200px] z-[1]">
+                <h1 className="text-[140px] scale-120 font-bold text-darkGray/40
+              leading-none">
+                Blvck Coffee
+                </h1>
+              </div>
             </div>
-            
+            {/* 3rd dive section */}
+            <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
+                <h1 className="opacity-0 hidden text-7xl font-bold leading-tight ml-14">
+                  Blvck Coffee
+                </h1>
+                
+                <div className="relative">
+                  <div className="relative z-10 space-y-4">
+                    <h1> Black Lifestyle Lovers, </h1>
+                    <h1 className="text-sm opacity-55 leading-loose">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Numquam molestiae fuga quis impedit suscipit sequi. Error
+                      aliquam, laborum voluptate voluptatum amet explicabo. 
+                      Commodi cumque beatae voluptate, incidunt nemo mollitia 
+                      tempore!
+                    </h1>
+                  </div>
 
-            {/* 3rd dive */}
+                  <div className="absolute -bottom-10 --10 w-[250px] 
+                h-[190px] bg-gray-700/25"></div>
+
+                </div>
+
+                <div className="absolute -top-1 -left-10 w-[250px] 
+                h-[190px] bg-gray-700/25"></div>
+
+              
+              </div>
           </div>
         </div>
       </section>
