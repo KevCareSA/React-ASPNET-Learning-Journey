2c24cabd0156846936b8d4a52619091cc7ab377b
KevCareSA <kev.mkbn2@gmail.com>
2025-07-02 20:46:07 +0200

    feat(hero): introduce framer-motion animations

--- demo-proj-1/src/components/Hero.jsx
+++ demo-proj-1/src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import BlackCoffee from "../assets/black.png";
 import BgImage from "../assets/bg-slate.png";
+import Navbar from "./Navbar";
+import {motion} from "framer-motion";
 
 
 const bgImage = {
@@ -19,18 +21,37 @@ const Hero = () => {
       <section className="min-h-[750px] w-full">
         <div className="container">
           {/* Navbar Section*/}
+          <Navbar />
           
-        {/* Hero Section div*/}
+          {/* Hero Section div*/}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
            place-items-center min-h-[850px]">
             <div className="grid grid-cols-1 bg-green-100 border border-black">
               {/* text section */}
               <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
-                <h1 className="text-7xl font-bold leading-tight ml-14">
+                <motion.h1 
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ 
+                  type: "spring", 
+                  stiffness: 100, 
+                  damping: 10,
+                  delay: 1.2,
+                 }}
+                className="text-7xl font-bold leading-tight ml-14">
                   Blvck Coffee
-                </h1>
+                </motion.h1>
                 
-                <div className="relative">
+                <motion.div className="relative" 
+                   initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ 
+                  type: "spring", 
+                  stiffness: 100, 
+                  damping: 10,
+                  delay: 1.2,
+                 }}
+                 >
                   <div className="relative z-10 space-y-4">
                     <h1> Black Lifestyle Lovers, </h1>
                     <h1 className="text-sm opacity-55 leading-loose">
@@ -45,7 +66,7 @@ const Hero = () => {
                   <div className="absolute -bottom-10 -left-10 w-[250px] 
                 h-[190px] bg-gray-700/25"></div>
 
-                </div>
+                </motion.div>
 
                 <div className="absolute -top-1 -left-10 w-[250px] 
                 h-[190px] bg-gray-700/25"></div>
@@ -71,14 +92,24 @@ const Hero = () => {
               </div>
             </div>
             {/* 3rd dive section */}
-            <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
+            <motion.div 
+              initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ 
+                  type: "spring", 
+                  stiffness: 100, 
+                  damping: 10,
+                  delay: 1.2,
+                 }}
+                 
+            className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
                 <h1 className="opacity-0 hidden text-7xl font-bold leading-tight ml-14">
                   Blvck Coffee
                 </h1>
                 
                 <div className="relative">
                   <div className="relative z-10 space-y-4">
-                    <h1> Black Lifestyle Lovers, </h1>
+                    <h1> The KevCare Design, </h1>
                     <h1 className="text-sm opacity-55 leading-loose">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Numquam molestiae fuga quis impedit suscipit sequi. Error
@@ -95,9 +126,7 @@ const Hero = () => {
 
                 <div className="absolute -top-1 -left-10 w-[250px] 
                 h-[190px] bg-gray-700/25"></div>
-
-              
-              </div>
+              </motion.div>
           </div>
         </div>
       </section>
