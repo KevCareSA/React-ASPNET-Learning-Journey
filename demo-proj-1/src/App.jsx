24fcae29ca90e331d103179d47f2b555f3e17f8e
KevCareSA <kev.mkbn2@gmail.com>
2025-07-03 19:41:27 +0200

    chore(app): integrate Service component into App

--- demo-proj-1/src/App.jsx
+++ demo-proj-1/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from './components/Hero';
+import Service from './components/Services';
 
 
 function App() {
   return (
     <div className="overflow-x-hidden"> 
       <Hero/>
+      <Service />
     </div>
   
   );
