0929843d20ddb743427f0bdcb769a42cb3998af3
KevCareSA <kev.mkbn2@gmail.com>
2025-07-03 19:39:21 +0200

    feat(components): add Services.jsx

---
+++ demo-proj-1/src/components/Services.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className='container my-16 space-y-4'>
+        {/* Header Section */}
+        <div className='text-center max-w-lg mx-auto space-y-2'>
+            <h1 className='text-3xl font-bold text-lightGray'>
+                Fresh and <span className='text-primary'>Quality Coffee</span>
+            </h1>
+            <p className='text-sm opacity-50'>
+                We offer a wide range of coffee products, 
+                from classic blends to unique flavors, 
+                all made with the finest ingredients.
+            </p>
+        </div>
+        {/* Card Section */}
+
+    </div>
+  )
+}
+
+export default Services
