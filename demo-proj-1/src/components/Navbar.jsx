fddfe80b387471cade712fa7182da9c684c72b2a
KevCareSA <kev.mkbn2@gmail.com>
2025-07-03 18:58:29 +0200

    feat(components): add Navbar.jsx

--- demo-proj-1/src/components/Navbar.jsx
+++ demo-proj-1/src/components/Navbar.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { motion } from 'framer-motion'
 
-const Navbar = () => {
+const Navbar = ({ sidebar, setSidebar}) => {
+    
   return (
-    <nav className="absolute top-0 left-0 w-full pt-10 text-white z-20">
+    <nav className="absolute top-0 left-0 w-full pt-10 text-white z-40">
         <div className="container">
             <div className='flex items-center justify-between'>
                 {/* logo section */}
-                <h1 className='text-2xl font-semibold uppercase'>
+                <motion.h1 
+                    initial={{ opacity: 0, y: -100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ 
+                    type: "spring", 
+                    stiffness: 100, 
+                    damping: 10,
+                    delay: 0.2,
+                    }}
+                    
+                    className='text-2xl font-semibold uppercase'>
                     <span className='text-primary'>KevCare's Coder </span> Coffee.
-                </h1>
+                </motion.h1>
                 {/* hamburger menu section */}
-                <div>
+                <motion.div
+                    initial={{ opacity: 0, y: -100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ 
+                    type: "spring", 
+                    stiffness: 100, 
+                    damping: 10,
+                    delay: 0.2,
+                    }}
+                    onClick={() => setSidebar(!sidebar)}
+                >
                     <GiHamburgerMenu className='text-3xl cursor-pointer' />
-                </div>
+                </motion.div>
             </div>
         </div>
     </nav>
