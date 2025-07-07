e9a48a172a3ce3f2f4c5a3855fc3420534164e84
KevCareSA <kev.mkbn2@gmail.com>
2025-07-07 19:20:24 +0200

    feat: add responsive order form to WhereToBuy component

--- demo-proj-1/src/components/WhereToBuy.jsx
+++ demo-proj-1/src/components/WhereToBuy.jsx
@@ -8,12 +8,31 @@ const WhereToBuy = () => {
         <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 
         place-items-center'>
             {/* form section */}
-            <div>
-
+            <div className='space-y-8'>
+                <h1 className='text-4xl font-bold text-darkGray font-serif'>
+                    Buy our coffee from anywhere!
+                </h1>
+                <div className='flex items-center gap-4'>
+                        <input type="text" placeholder='Enter your name' 
+                        className='input-style w-full lg:w-[150px]'/>
+                        <input type="email" placeholder='Enter your email' 
+                        className='input-style w-full' />
+                </div>
+                <div className='flex items-center gap-4'>
+                        <input type="text" placeholder='Country' 
+                        className='input-style w-full '/>
+                        <input type="email" placeholder='Zip Code' 
+                        className='input-style w-full lg:w-[150px]' />  
+                </div>
+                <button type="submit" 
+                        className='primary-btn'>
+                            Order Now
+                </button>
             </div>
             {/* world map section */}
             <div className='col-span-2'>
-              <img src={WorldMap} alt="World Map" />
+              <img src={WorldMap} alt="World Map" 
+              className='w-full sm:w-[500px] mx-auto'/>
             </div>
         </div>
     </div>
