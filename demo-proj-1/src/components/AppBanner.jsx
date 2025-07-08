e561f06b1b987077a54e7fa06b70fd07a15503fa
KevCareSA <kev.mkbn2@gmail.com>
2025-07-08 02:03:54 +0200

    feat: animate AppBanner section with framer motion

--- demo-proj-1/src/components/AppBanner.jsx
+++ demo-proj-1/src/components/AppBanner.jsx
@@ -21,21 +21,71 @@ const AppBanner = () => {
         sm:items-center sm:justify-end rounded-xl'>
             <div>
                <div className='space-y-6 max-w-xl mx-auto'>
-                <h1 className='text-2xl text-center sm:text-4xl font-semibold'>
+                <motion.h1 
+                     initial={{ opacity: 0, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ 
+                        type: 'spring',
+                        stiffness: 100,
+                        damping: 20,
+                        delay: 0.2,
+                     }} 
+                    
+                    className='text-2xl text-center sm:text-4xl font-semibold'>
                     Download Our App
-                </h1>
-                <p className='text-center sm:px-20'>
+                </motion.h1>
+                <motion.p 
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ 
+                        type: 'spring',
+                        stiffness: 100,
+                        damping: 20,
+                        delay: 0.3,
+                     }}
+                    
+                    className='text-center sm:px-20'>
                     Get your daily dose of caffeine
-                </p>
+                </motion.p>
                 {/* image links */}
-                <div className='flex items-center justify-center gap-4'>
-                    <a href="#">
+                <motion.div 
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ 
+                        type: 'spring',
+                        stiffness: 100,
+                        damping: 20,
+                        delay: 0.3,
+                     }}
+
+                    className='flex items-center justify-center gap-4'>
+                    <motion.a href="#"
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{
+                            type: 'spring',
+                            stiffness: 100,
+                            damping: 20,
+                            delay: 0.4,
+                        }}
+                        className='max-w-[150px] sm:max-w-[120px]
+                        md:max-w-[200px]'>
                         <img src={Appstore} alt="App Store" />
-                    </a>
-                    <a href="#">
+                    </motion.a>
+                    <motion.a href="#"
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{
+                            type: 'spring',
+                            stiffness: 100,
+                            damping: 20,
+                            delay: 0.5,
+                        }}
+                        className='max-w-[150px] sm:max-w-[120px]
+                        md:max-w-[200px]'>
                         <img src={Playstore} alt="Play Store" />
-                    </a>
-                </div>
+                    </motion.a>
+                </motion.div>
                </div>
             </div>
         </div>
