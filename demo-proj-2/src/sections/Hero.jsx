b0dffbcdddf01864034754451aa68b6c0a6e462b
KevCareSA <kev.mkbn2@gmail.com>
2025-07-17 22:08:34 +0200

    feat: update Hero section with background image and responsive layout

--- demo-proj-2/src/sections/Hero.jsx
+++ demo-proj-2/src/sections/Hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import backgroundImage from '../assets/homeimg.webp'
+//import heroimg from '../assets/heroimg.png'
+//import {slideUpVariants, zoomInVariants} from './animation'
+//import { motion } from 'framer-motion'
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div id='hero' className='bg-black w-full lg:h-[700px] h-fit 
+      m-auto pt-[60px] lg:pt-[0px] lg:px-[150px] px-[20px] flex justify-between 
+      items-center lg:flex-row flex-col lg:gap-5 gap-[50px] bg-cover bg-center' 
+      style={{ backgroundImage: `url(${backgroundImage})` }}>
+    </div>
   )
 }
 
