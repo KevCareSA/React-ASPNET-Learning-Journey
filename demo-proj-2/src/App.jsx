dfb437721e111acc7ccc79b88e9c0c92f0409108
KevCareSA <kev.mkbn2@gmail.com>
2025-07-17 22:08:12 +0200

    feat: integrate all section components into App.jsx layout

--- demo-proj-2/src/App.jsx
+++ demo-proj-2/src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import React  from 'react'
 import './index.css'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Portfolio from './sections/Portfolio'
+import { Work } from './sections/Work'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
+import About from './sections/About'
+import Service from './sections/Service'
+import Testimonials from './sections/Testimonials'
+
 
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-green-600">
-        Tailwind is working!
-      </h1>
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <Service />
+      <Portfolio />
+      <Work />
+      <Testimonials />
+      <Contact />
+      <Footer />
+      
     </div>
   );
 }
