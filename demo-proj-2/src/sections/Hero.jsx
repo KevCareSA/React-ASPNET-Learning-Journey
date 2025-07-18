18688863bc147b095b9d95e39f1a96b8105ee28c
KevCareSA <kev.mkbn2@gmail.com>
2025-07-18 10:08:58 +0200

    feat: animate Hero section with framer motion

--- demo-proj-2/src/sections/Hero.jsx
+++ demo-proj-2/src/sections/Hero.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import backgroundImage from '../assets/homeimg.webp'
 //import heroimg from '../assets/heroimg.png'
-//import {slideUpVariants, zoomInVariants} from './animation'
-//import { motion } from 'framer-motion'
+import {slideUpVariants, zoomInVariants} from './animation'
+import { motion } from 'framer-motion'
 
 
 const Hero = () => {
@@ -11,6 +11,23 @@ const Hero = () => {
       m-auto pt-[60px] lg:pt-[0px] lg:px-[150px] px-[20px] flex justify-between 
       items-center lg:flex-row flex-col lg:gap-5 gap-[50px] bg-cover bg-center' 
       style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <motion.div
+        initial={'hidden'}
+        whileInView={'visible'}
+        variants={slideUpVariants}
+        className='lg:w-[60%] w-full flex flex-col justify-center items-start
+        lg:gap-8 gap-4'
+        >
+          <motion.h1
+          variants={zoomInVariants}
+          className='text-yellw lg:text-[60px] text-2xl'>
+            WE ARE BUILDERS
+          </motion.h1>
+
+          <motion.h1>
+            We Build Your Dreams
+          </motion.h1>
+        </motion.div>
     </div>
   )
 }
