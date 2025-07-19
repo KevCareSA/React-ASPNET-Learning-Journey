6785729240b399df472928f57784556b76b9f79f
KevCareSA <kev.mkbn2@gmail.com>
2025-07-19 22:48:43 +0200

    feature: Add framer animations to Hero Section

--- demo-proj-2/src/sections/Hero.jsx
+++ demo-proj-2/src/sections/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import backgroundImage from '../assets/homeimg.webp'
 //import heroimg from '../assets/heroimg.png'
-import {slideUpVariants, zoomInVariants} from './animation'
+import {slideUpVariants, ZoomInVariants} from './animation'
 import { motion } from 'framer-motion'
+
 
 
 const Hero = () => {
@@ -19,14 +20,42 @@ const Hero = () => {
         lg:gap-8 gap-4'
         >
           <motion.h1
-          variants={zoomInVariants}
-          className='text-yellw lg:text-[60px] text-2xl'>
+          variants={slideUpVariants}
+          className='text-yellow-500 text-2xl'>
             WE ARE BUILDERS
           </motion.h1>
 
-          <motion.h1>
+          <motion.h1 
+            className='text-white text-[40px] font-bold'>
             We Build Your Dreams
           </motion.h1>
+          <div className='w-[400px] h-[6px] bg-yellow-500'>
+            <p className='text-white text-[20px]'>Building And Constructing Dreams Together</p>
+          </div>
+
+          <motion.div
+            initial={'hidden'}
+            whileInView={'visible'}
+            variants={slideUpVariants}
+            className='flex justify-center items-center gap-5'
+        >
+            <motion.button
+              variants={ZoomInVariants}
+              className='bg-yellow-500 hover:bg-white hover:text-black transition-all duration-300 
+              px-4 py-1.5 rounded-lg text-white font-bold'
+              >
+                READ MORE
+            </motion.button>
+            <motion.button
+              variants={ZoomInVariants}
+              className='bg-white hover:border-yellow-500 
+              hover:text-yellow-500 transition-all duration-300 border-2
+              px-4 py-1.5 rounded-lg text-black font-bold'
+              >
+                CONTACT US
+            </motion.button>
+          </motion.div>
+
         </motion.div>
     </div>
   )
