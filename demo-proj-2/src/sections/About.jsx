ca11df56541f7617563135b0a2a9f86c48008e5d
KevCareSA <kev.mkbn2@gmail.com>
2025-07-21 22:11:57 +0200

    feat: add animated heading layout to About section using Framer Motion

--- demo-proj-2/src/sections/About.jsx
+++ demo-proj-2/src/sections/About.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { slideUpVariants } from './animation'
+//import { slideUpVariants, ZoomInVariants } from './animation'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='lg:w-[80%] w-[90%] m-auto py-[60px] flex 
+    lg:flex-row flex-col justify-between items-start gap-[50px]' id='about'>
+      <motion.div 
+        initial={'hidden'}
+        whileInView={'visible'}
+        variants={{slideUpVariants}}
+        className='lg:w-[50%] w-full flex flex-col justify-center items-start
+        gap-5'>
+        <motion.h1 
+          variants={slideUpVariants}
+          className='text-yellow-500 text-2xl'>
+          WELCOME TO
+        </motion.h1>
+        <motion.h1 
+          variants={slideUpVariants}
+          className='text-white text-[40px] font-bold'>
+          About Us
+        </motion.h1>
+
+      </motion.div>
+
+    </div>
   )
 }
 
