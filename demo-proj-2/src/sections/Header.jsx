4cfb543969cfc55fa33b7edccd61adf46346ba99
KevCareSA <kev.mkbn2@gmail.com>
2025-07-28 18:24:27 +0200

    feat: build responsive Header with toggle menu and smooth scroll links

--- demo-proj-2/src/sections/Header.jsx
+++ demo-proj-2/src/sections/Header.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { FaXmark, FaBars } from 'react-icons/fa'
+//import { motion } from 'framer-motion'
+import { Link} from 'react-scroll'
+
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  }
+
+  const navItems = [
+    { link: 'Home', to: 'home' },
+    { link: 'About', to: 'about' },
+    { link: 'Services', to: 'services' },
+    { link: 'Projects', to: 'projects' },
+    { link: 'Contact', to: 'contact' },
+  ];
+
+  
+
   return (
-    <div>Header</div>
+    <nav className='w-full bg-white justify-between items-center gap-1 
+    lg:px-16 px-4 sticky top-0 z-50 shadow-md'>
+      <h1 className='text-black md:text-4xl text-3xl font-bold font-rubik 
+      cursor-pointer'>
+        Sally 
+        <span>Construction And Projects</span>
+
+      </h1>
+    </nav>
   )
 }
 
