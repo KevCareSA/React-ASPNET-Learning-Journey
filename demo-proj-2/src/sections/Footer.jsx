2fbead17ee1acaeed0c691cf8b7864cf7c3cb0fb
KevCareSA <kev.mkbn2@gmail.com>
2025-07-31 19:19:32 +0200

    feat: complete footer with copyright and scroll-to-top button

--- demo-proj-2/src/sections/Footer.jsx
+++ demo-proj-2/src/sections/Footer.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-scroll";
+import { FaCopyright } from "react-icons/fa6";
+import { FaArrowUp } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div
+        className="bg-black text-white flex justify-center items-center 
+      gap-2 p-5"
+      >
+        <FaCopyright className="fill-yellow-500 lg:size-5 size-8" />
+        <p className="text-lg text-center">
+          {new Date().getFullYear()} Sally Construction And Projects. All rights
+          reserved.
+        </p>
+      </div>
 
-export default Footer
+      {/* scroll to top button */}
+      <div
+        id="icon-box"
+        className="bg-yellow-500 text-black p-3 rounded-full
+      hover:bg-black hover:text-white cursor-pointer fixed lg:bottom-6 
+      right-6 bottom-6"
+      >
+        <Link to="hero" spy={true} offset={-100} smooth={true}>
+          <FaArrowUp className="size-6" />
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
